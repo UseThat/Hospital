32cfc8b8ff6906f497ad088fef6e653e977a2d3e
Mateusz Wygoda <mateusz.wygoda@gmail.com>
2022-02-12 23:00:36 +0100

    Add Hospital.java

--- src/app/HospitalApp.java
+++ src/app/HospitalApp.java
@@ -1,4 +1,38 @@
 package app;
 
+import logic.Hospital;
+
 public class HospitalApp {
+    public static void main(String[] args) {
+        Hospital hospital = new Hospital();
+        int number;
+        do {
+            actionList();
+            number = hospital.switchNumber();
+            switch (number){
+                case 0:
+                    System.out.println("Bye bye!");
+                    break;
+                case 1:
+                    hospital.addPatient();
+                    break;
+                case 2:
+                    hospital.printPatient();
+                    break;
+                default:
+                    System.out.println("No option choose");
+            }
+        }while(number!=0);
+        hospital.close();
+
+
+
+    }
+    private static void actionList(){
+        System.out.println("0 - exit");
+        System.out.println("1 - add patient");
+        System.out.println("2 - print patient");
+    }
+
+
 }
--- src/logic/Hospital.java
+++ src/logic/Hospital.java
@@ -1,4 +1,59 @@
 package logic;
 
+import model.Patient;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Hospital {
+    private final int maxPatient = 2;
+    private Patient[] patients = new Patient[maxPatient];
+    private int patientCounter;
+    private Scanner scanner;
+
+    public Hospital(){
+        scanner = new Scanner(System.in);
+    }
+
+    public void close(){
+        scanner.close();
+    }
+
+    public void addPatient(){
+        if(patientCounter < maxPatient){
+            createPatient();
+            patientCounter ++;
+            System.out.println("Patient created!");
+        }else{
+            System.out.println("Patient list full");
+        }
+    }
+
+    private void createPatient(){
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine();
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine();
+        System.out.print("Document id: ");
+        String document = scanner.nextLine();
+        System.out.print("Age: ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        patients[patientCounter] = new Patient(firstName,lastName, document,age);
+    }
+
+    public void printPatient(){
+        for (Patient patient : patients) {
+            System.out.println(patient);
+        }
+    }
+
+    public int switchNumber(){
+        int number  = scanner.nextInt();
+        scanner.nextLine();
+        return number;
+    }
+
+
+
 }
